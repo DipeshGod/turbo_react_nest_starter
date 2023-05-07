b9004d54f00f53c15967f4a182a37d93e93ce514
Dipesh <dipesh@heubert.com>
2023-05-07 16:51:40 +0545

    add tanstack router

--- apps/product_admission/src/main.tsx
+++ apps/product_admission/src/main.tsx
@@ -1,9 +1,53 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
+import {
+  Link,
+  Outlet,
+  RootRoute,
+  Route,
+  Router,
+  RouterProvider,
+} from '@tanstack/router';
+import Login from './routes/login.tsx';
+
+function Root() {
+  return (
+    <>
+      <div>
+        <Link to="/">Home</Link>
+      </div>
+      <hr />
+      <Outlet />
+    </>
+  );
+}
+
+// Create a root route
+const rootRoute = new RootRoute({
+  component: Root,
+});
+
+const indexRoute = new Route({
+  getParentRoute: () => rootRoute,
+  path: '/',
+  component: Login,
+});
+
+// Create the route tree using your routes
+const routeTree = rootRoute.addChildren([indexRoute]);
+
+// Create the router using your route tree
+const router = new Router({ routeTree });
+
+// Register your router for maximum type safety
+declare module '@tanstack/router' {
+  interface Register {
+    router: typeof router;
+  }
+}
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
