f4b2c9fdc755d4ac8634404315b3273890d2c47c
Dipesh <dipesh@heubert.com>
2023-05-19 21:11:39 +0545

    replace loading text with skeleton

--- apps/lyra/src/routes/wrapper/PublicWrapper.tsx
+++ apps/lyra/src/routes/wrapper/PublicWrapper.tsx
@@ -1,18 +1,11 @@
-// import { ReactNode } from "react";
-import { ReactElement } from "react";
-import { Navigate } from "react-router-dom";
+import { ReactElement } from 'react';
+import { Navigate } from 'react-router-dom';
 
 export const PublicWrapper = ({ component }: { component: ReactElement }) => {
-    const token = localStorage.getItem('jwt_token');
-    console.log(token);
-    if (token) {
-        return (
-            <Navigate to="/" replace={true} />
-        )
-    }
-    return (
-        <>
-            {component}
-        </>
-    )
-}
+  const token = localStorage.getItem('jwt_token');
+  console.log(token);
+  if (token) {
+    return <Navigate to="/" replace={true} />;
+  }
+  return <>{component}</>;
+};
